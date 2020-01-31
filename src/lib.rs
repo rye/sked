e3d8b543f397004149fd44bd17c77e452d202c43
Kristofer Rye <kristofer.rye@gmail.com>
2020-01-31 08:58:01 -0600

    Split out Part into its own module
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -63,44 +63,8 @@ impl<Tz: TimeZone> Exception<Tz> {
 	}
 }
 
-#[allow(dead_code)]
-#[derive(Debug)]
-pub struct Part<Tz: TimeZone> {
-	open: Option<TimeSpecifier<Tz>>,
-	close: Option<TimeSpecifier<Tz>>,
-	notes: Vec<String>,
-}
-
-impl<Tz: TimeZone> Default for Part<Tz> {
-	fn default() -> Self {
-		Self {
-			open: None,
-			close: None,
-			notes: Vec::new(),
-		}
-	}
-}
-
-impl<Tz: TimeZone> Part<Tz> {
-	pub fn new() -> Self {
-		Self::default()
-	}
-
-	pub fn open(mut self, ts: TimeSpecifier<Tz>) -> Self {
-		self.open = Some(ts);
-		self
-	}
-
-	pub fn close(mut self, ts: TimeSpecifier<Tz>) -> Self {
-		self.close = Some(ts);
-		self
-	}
-
-	pub fn note(mut self, note: &str) -> Self {
-		self.notes.push(note.to_string());
-		self
-	}
-}
+mod part;
+pub use part::*;
 
 mod schedule;
 pub use schedule::*;

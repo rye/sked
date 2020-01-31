493c4d49f6d3bc3bb1aac8848b931973692e2f94
Kristofer Rye <kristofer.rye@gmail.com>
2020-01-31 08:59:33 -0600

    Split out Exception into its own module
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -24,44 +24,8 @@ pub enum Status {
 	Closed { reason: Option<String> },
 }
 
-#[allow(dead_code)]
-#[derive(Debug)]
-pub struct Exception<Tz: TimeZone> {
-	effect: Status,
-	effective: Option<TimeSpecifier<Tz>>,
-	expires: Option<TimeSpecifier<Tz>>,
-}
-
-impl<Tz: TimeZone> Default for Exception<Tz> {
-	fn default() -> Self {
-		Self {
-			effect: Status::Closed { reason: None },
-			effective: None,
-			expires: None,
-		}
-	}
-}
-
-impl<Tz: TimeZone> Exception<Tz> {
-	pub fn new() -> Self {
-		Self::default()
-	}
-
-	pub fn effect(mut self, effect: Status) -> Self {
-		self.effect = effect;
-		self
-	}
-
-	pub fn effective(mut self, effective: TimeSpecifier<Tz>) -> Self {
-		self.effective = Some(effective);
-		self
-	}
-
-	pub fn expires(mut self, expires: TimeSpecifier<Tz>) -> Self {
-		self.expires = Some(expires);
-		self
-	}
-}
+mod exception;
+pub use exception::*;
 
 mod part;
 pub use part::*;

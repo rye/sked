0317fccc6438662b238362534bfcfa08f491a39e
Kristofer Rye <kristofer.rye@gmail.com>
2020-06-09 16:35:50 -0500

    Instances: Implement Specifier::Weekly logic
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/specifier.rs
+++ src/specifier.rs
@@ -33,7 +33,40 @@ impl<'iteration, Tz: TimeZone> Iterator for Instances<'iteration, Tz> {
 			}
 			Specifier::Exact(dt) if dt == &self.basis => None,
 			Specifier::Exact(_) => panic!(),
-			Specifier::Weekly { .. } => todo!(),
+			Specifier::Weekly { day, time } => {
+				let specifier_day: chrono::Weekday = day.parse().expect("invalid day specifier");
+				let specifier_time: chrono::NaiveTime = NaiveTime::parse_from_str(time, "%H:%M")
+					.or(NaiveTime::parse_from_str(time, "%H:%M:%S"))
+					.expect("invalid time specifier");
+
+				// If the basis weekday is the same as the specifier, then return today's instance
+				if self.basis.weekday() == specifier_day {
+					let instance = self.basis.date().and_time(specifier_time).unwrap();
+
+					self.basis = self.basis.to_owned() + chrono::Duration::weeks(1);
+
+					Some(instance)
+				} else {
+					let basis_weekday: i64 = self.basis.date().weekday().num_days_from_monday().into();
+					let next_instance_weekday: i64 = specifier_day.num_days_from_monday().into();
+					let mut difference = next_instance_weekday - basis_weekday;
+
+					// We must add days to get to the next updates.
+					if difference < 0 {
+						difference += 7;
+					}
+
+					let offset = chrono::Duration::days(difference.into());
+
+					let instance = (self.basis.date() + offset)
+						.and_time(specifier_time)
+						.unwrap();
+
+					self.basis = self.basis.to_owned() + chrono::Duration::weeks(1);
+
+					Some(instance)
+				}
+			}
 			Specifier::Daily { time } => {
 				let specifier_time: chrono::NaiveTime = NaiveTime::parse_from_str(time, "%H:%M")
 					.or(NaiveTime::parse_from_str(time, "%H:%M:%S"))

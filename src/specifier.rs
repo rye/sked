b2a9fa2130fa2ed35cd8be7db1402f2814adafe9
Kristofer Rye <kristofer.rye@gmail.com>
2020-06-09 17:08:48 -0500

    specifier: Add tests for specifier instance generation
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/specifier.rs
+++ src/specifier.rs
@@ -92,3 +92,78 @@ impl<Tz: TimeZone> Specifier<Tz> {
 		self.instances(basis).take(n).collect()
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	mod specifier {
+		use crate::Specifier;
+		use chrono::DateTime;
+
+		#[test]
+		fn instances_exact() {
+			let now = chrono::Local::now();
+			let s = Specifier::Exact(now);
+			assert_eq!(
+				s.instances(chrono::Local::now())
+					.collect::<Vec<DateTime<chrono::Local>>>(),
+				vec![now]
+			);
+		}
+
+		#[test]
+		fn instances_daily() {
+			let t_ref = DateTime::parse_from_rfc3339("2020-01-16T10:15:00-05:00").unwrap();
+			let s = Specifier::Daily {
+				time: "07:00".to_string(),
+			};
+			assert_eq!(
+				s.instances(t_ref)
+					.take(3)
+					.collect::<Vec<DateTime<chrono::FixedOffset>>>(),
+				vec![
+					DateTime::parse_from_rfc3339("2020-01-16T07:00:00-05:00").unwrap(),
+					DateTime::parse_from_rfc3339("2020-01-17T07:00:00-05:00").unwrap(),
+					DateTime::parse_from_rfc3339("2020-01-18T07:00:00-05:00").unwrap(),
+				]
+			);
+		}
+
+		#[test]
+		fn instances_weekly_start_after_date() {
+			let t_ref = DateTime::parse_from_rfc3339("2020-01-16T10:15:00-05:00").unwrap();
+			let s = Specifier::Weekly {
+				day: "Tuesday".to_string(),
+				time: "07:00".to_string(),
+			};
+			assert_eq!(
+				s.instances(t_ref)
+					.take(3)
+					.collect::<Vec<DateTime<chrono::FixedOffset>>>(),
+				vec![
+					DateTime::parse_from_rfc3339("2020-01-21T07:00:00-05:00").unwrap(),
+					DateTime::parse_from_rfc3339("2020-01-28T07:00:00-05:00").unwrap(),
+					DateTime::parse_from_rfc3339("2020-02-04T07:00:00-05:00").unwrap(),
+				]
+			);
+		}
+
+		#[test]
+		fn instances_weekly_start_on_same_date() {
+			let t_ref = DateTime::parse_from_rfc3339("2020-01-14T10:15:00-05:00").unwrap();
+			let s = Specifier::Weekly {
+				day: "Tuesday".to_string(),
+				time: "07:00".to_string(),
+			};
+			assert_eq!(
+				s.instances(t_ref)
+					.take(3)
+					.collect::<Vec<DateTime<chrono::FixedOffset>>>(),
+				vec![
+					DateTime::parse_from_rfc3339("2020-01-14T07:00:00-05:00").unwrap(),
+					DateTime::parse_from_rfc3339("2020-01-21T07:00:00-05:00").unwrap(),
+					DateTime::parse_from_rfc3339("2020-01-28T07:00:00-05:00").unwrap(),
+				]
+			);
+		}
+	}
+}
